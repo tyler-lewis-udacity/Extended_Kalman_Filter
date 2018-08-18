82aa7389ee647f42bcd8d88ba63f7f8c9d7ba6d8
Tyler Lewis <tyler.lewis.udacity@gmail.com>
2018-08-17 20:09:00 -0500

    work in progress

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -55,11 +55,6 @@ FusionEKF::FusionEKF() {
              0, 1, 0, 0,
              0, 0, 1000, 0,
              0, 0, 0, 1000;
-
-  // set the acceleration noise components
-  noise_ax = 9; //aka: (sigma_ax)^2. Value of '9' provided in quiz from Lesson 5, Section 13
-  noise_ay = 9; //aka: (sigma_ay)^2. Value of '9' provided in quiz from Lesson 5, Section 13
-  
 }
 
 /**
@@ -84,7 +79,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1; // last 2 elements very important for RMSE
+    ekf_.x_ << 1, 1, 5.2, 0; // last 2 elements very important for RMSE
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
@@ -92,7 +87,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       float rho     = measurement_pack.raw_measurements_(0);
       float phi     = measurement_pack.raw_measurements_(1);
-      float rho_dot = measurement_pack.raw_measurements_(2);
+      //float rho_dot = measurement_pack.raw_measurements_(2);
    
       ekf_.x_(0) = rho*cos(phi);// set ekf_.x_(0) to ro*cos(phi) (...23.41 in walkthrough video)
       ekf_.x_(1) = rho*sin(phi);// set ekf_.x_(1) to ro*sin(phi)
@@ -135,9 +130,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt_3 = dt_2*dt;// dt^3
   float dt_4 = dt_3*dt;// dt^4
 
+  // set the acceleration noise components
+  float noise_ax = 9; //aka: (sigma_ax)^2. Value of '9' provided in quiz from Lesson 5, Section 13
+  float noise_ay = 9; //aka: (sigma_ay)^2. Value of '9' provided in quiz from Lesson 5, Section 13
+  
   // Modify the F matrix so that the time is integrated (Lesson 5, Section 9)
-  ekf_F_(0,2) = dt;
-  ekf_F_(1,3) = dt;
+  ekf_.F_(0,2) = dt;
+  ekf_.F_(1,3) = dt;
 
   // Set the process covariance matrix "Q" (Lesson 5, Section 10)
   ekf_.Q_ = MatrixXd(4,4);
@@ -156,14 +155,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    TODO:
      * Use the sensor type to perform the update step.
      * Update the state and covariance matrices.
-   */
-  /* --------------------- BOOKMARK ------------------*/
-
+  */
+  
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    Tools tools;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+
+    ekf_.R_ = R_radar_;
+    
+    ekf_.Update(measurement_pack.raw_measurements_);
+
   } else {
     // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_radar_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_*x_; //(...Lesson 5, Section 8)
+  MatrixXd Ft = F_.transpose();
+  P_ = F_*P_ * Ft + Q_; 
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +35,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  // Lesson 5, Section 7
+  VectorXd y = z - H_*x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_*P_*Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_*Ht*Si;
+
+  // new state vector
+  x_ = x_ + (K*y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K*H_)*P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,33 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float x = x_(0);
+  float y2 = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float rho = sqrt((x*x) + (y2*y2));
+  float phi = atan2(y2,x);
+  //float rho_dot = ((x*vx) + (y2*vy))/rho;
+  float rho_dot;
+  if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+  } else {
+    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  }
+  
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_*P_*Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_*Ht*Si;
+
+  // new state vector
+  x_ = x_ + (K*y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K*H_)*P_;
 }
